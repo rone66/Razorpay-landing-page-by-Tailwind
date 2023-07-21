c5565ff0b0398e867a961528929bee96475f9186
Arnab <rnbpaul56@gmail.com>
2023-07-21 11:57:59 +0530

    version 1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+     
+    },
+    colors:{
+      'darkBlue':"#02042B",
+      'white':'#ffff',
+      'blue':'#0B72E7',
+      'lightBlue':'#2B84EA',
+      'green':'#61CEA6',
+      'grey':'#526582',
+    }
+  },
+  plugins: [],
+}
+
